47fb994f38b5b6009dd9e0a8a7a0402ddb70f5bd
mkuzak <mateusz.kuzak@gmail.com>
2014-09-07 18:56:41 +0200

    Use python geojson. Skip properties
    Properties contain not allowed values,
    need to clean it up and bring them back.

--- versailes2geojson.py
+++ versailes2geojson.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import json
+import geojson
+from geojson import FeatureCollection, Feature, Point
 
 # read in original data
 csv = pd.read_csv("versaille_stock.csv", sep="\t")
@@ -12,20 +13,18 @@ props = [p for p in props if p not in ["Longitude",
 features = []
 for row in csv.iterrows():
     accession = row[1]
-    feature = {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [accession["Latitude"],
-                            accession["Longitude"]]
-        }
-    }
+    lat = accession["Latitude"]
+    lon = accession["Longitude"]
     # automatically populate accession properties
     feature_props = {p: accession[p] for p in props}
-    feature["properties"] = feature_props
-    features.append(feature)
-feature_collection = {"type": "FeatureCollection",
-                      "features": features}
 
-with open("accession_locations.json", "w") as f:
-    json.dump(feature_collection, f)
+    f = Feature(
+            geometry = Point((lat,lon))
+        )
+
+    features.append(f)
+
+fc = FeatureCollection(features)
+
+with open("accession_locations.json", "w") as fh:
+    geojson.dump(fc, fh)
