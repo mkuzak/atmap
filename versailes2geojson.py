614a6a4318fb7ee2415f079ed0b84e6cf213826c
mkuzak <mateusz.kuzak@gmail.com>
2014-09-08 10:12:07 +0200

    Add feature properties.

--- versailes2geojson.py
+++ versailes2geojson.py
@@ -5,6 +5,9 @@ from geojson import FeatureCollection, Feature, Point
 # read in original data
 csv = pd.read_csv("versaille_stock.csv", sep="\t")
 
+# Pandas puts NaN in empty fields
+# swap NaN for empty strings
+csv = csv.fillna("")
 # fetch properties, remove Long and Lat
 props = list(csv.columns.values)
 props = [p for p in props if p not in ["Longitude",
@@ -19,7 +22,8 @@ for row in csv.iterrows():
     feature_props = {p: accession[p] for p in props}
 
     f = Feature(
-            geometry = Point((lon,lat))
+            geometry = Point((lon,lat)),
+            properties = feature_props
         )
 
     features.append(f)
